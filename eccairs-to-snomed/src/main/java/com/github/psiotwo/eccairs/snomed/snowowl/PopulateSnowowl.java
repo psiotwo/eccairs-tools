95b4bdba4ecff12d608c393bffb31f8031053c68
Petr Křemen <petr.kremen@gmail.com>
2021-07-10 23:51:38 +0200

    Refactoring to modules

---
+++ eccairs-to-snomed/src/main/java/com/github/psiotwo/eccairs/snomed/snowowl/PopulateSnowowl.java
@@ -0,0 +1,15 @@
+package com.github.psiotwo.eccairs.snomed.snowowl;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.psiotwo.eccairs.snomed.PopulateSnomedServer;
+import kong.unirest.UnirestException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PopulateSnowowl {
+
+    public static void main(String[] args) throws UnirestException, JsonProcessingException {
+        final SnowowlApi api = new SnowowlApi(args[0]);
+        new PopulateSnomedServer(api, args[1], args[2]);
+    }
+}
