3577fb82ead23554502ee79b1e688f0e805fac2a
Petr Křemen <petr.kremen@gmail.com>
2021-09-13 16:43:13 +0200

    code cleanup

--- eccairs-to-rdf/src/test/java/com/github/psiotwo/eccairs/rdf/EccairsTaxonomyToRdfTest.java
+++ eccairs-to-rdf/src/test/java/com/github/psiotwo/eccairs/rdf/EccairsTaxonomyToRdfTest.java
@@ -12,14 +12,14 @@ import org.junit.jupiter.api.Test;
 
 public class EccairsTaxonomyToRdfTest {
 
-    private EccairsDictionary dictionary = new EccairsDictionary()
+    private final EccairsDictionary dictionary = new EccairsDictionary()
         .setTaxonomy("ECCAIRS Aviation")
         .setVersion("3.4.0.2")
         .setLanguage("English")
         .setKey("00d4fe6f-f522-42a8-801f-65fd2a58af31")
         .setEntities(Collections.emptyList());
 
-    private EccairsEntity entity = new EccairsEntity()
+    private final EccairsEntity entity = new EccairsEntity()
         .setId(1)
         .setDescription("Description of 1")
         .setExplanation("Explanation of 1")
@@ -28,10 +28,10 @@ public class EccairsTaxonomyToRdfTest {
     @Test
     public void exporterWritesTaxonomyNameCorrectly() {
         final EccairsDictionary dictionary = this.dictionary;
-        final EccairsTaxonomyToRdf r = new EccairsTaxonomyToRdf("http://test.org/", dictionary);
+        final EccairsTaxonomyToRdf r = new EccairsTaxonomyToRdf("https://test.org/", dictionary);
         final OntModel model = r.transform();
         Assertions.assertEquals(1, model.listOntologies().toList().size());
-        Assertions.assertEquals("http://test.org/eccairs/aviation-3.4.0.2",
+        Assertions.assertEquals("https://test.org/eccairs/aviation-3.4.0.2",
             model.listOntologies().next().getURI());
     }
 
@@ -39,7 +39,7 @@ public class EccairsTaxonomyToRdfTest {
     public void exporterWritesEntityAttributes() {
         final EccairsDictionary dictionary = this.dictionary;
         dictionary.setEntities(Collections.singletonList(entity));
-        final EccairsTaxonomyToRdf r = new EccairsTaxonomyToRdf("http://test.org/", dictionary);
+        final EccairsTaxonomyToRdf r = new EccairsTaxonomyToRdf("https://test.org/", dictionary);
         final OntModel model = r.transform();
         final List<Resource> entities =
             model.listSubjectsWithProperty(RDF.type, model.getOntClass(Vocabulary.s_c_entity))
