b555b3d0c0dcee1692763f0617abf21785fc979b
Petr Křemen <petr.kremen@gmail.com>
2021-10-09 22:16:02 +0200

    [#6] mapping of ECCAIRS terms to the version-agnostic IRIs

--- eccairs-to-rdf/src/main/java/com/github/psiotwo/eccairs/rdf/EccairsTaxonomyToRdf.java
+++ eccairs-to-rdf/src/main/java/com/github/psiotwo/eccairs/rdf/EccairsTaxonomyToRdf.java
@@ -11,7 +11,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +24,9 @@ import org.apache.jena.ontology.ObjectProperty;
 import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.Ontology;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.ReifiedStatement;
 import org.apache.jena.rdf.model.Resource;
@@ -31,14 +36,40 @@ import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.vocabulary.RDFS;
 
+/**
+ * Transforms ECCAIRS taxonomy into an RDF dataset. It generates:
+ * - main model with the RDF version of the ECCAIRS XML file
+ * - mapping to the version agnostic IRIs. The mapping is currently only generated for entities, attributes and value lists,
+ * BUT NOT VALUES
+ */
 @Slf4j
 public class EccairsTaxonomyToRdf {
 
-    private OntModel model;
+    /**
+     * Ab RDF dataset with ECCAIRS taxonomy.
+     */
+    private Dataset dataset;
+
+    /**
+     * An Eccairs dictionary.
+     */
     private final EccairsDictionary dictionary;
 
+    /**
+     * Base Ontology IRI.
+     */
     private String base;
+
+    /**
+     * Taxonomy language.
+     */
     private String lang;
+
+    private OntModel model;
+    private Model mapping;
+
+    public String mainGraphIri;
+    public String mappingGraphIri;
 
     /**
      * Creates a new serializer of ECCAIRS to RDF.
@@ -58,28 +89,44 @@ public class EccairsTaxonomyToRdf {
             + "-" + dictionary.getVersion().replace(".", "_");
     }
 
+    private Map<String, String> setupNamespaces() {
+        final Map<String, String> namespaces = new HashMap<>();
+        namespaces.put("e-m", Vocabulary.ONTOLOGY_IRI_model + "/");
+        namespaces.put("e-a", getOntologyIriBase());
+        namespaces.put(generateOntologyPrefix(dictionary), getVersionedOntologyIriBase());
+        return namespaces;
+    }
+
     /**
      * Transforms an ECCAIRS taxonomy into RDF.
      */
-    public OntModel transform() {
+    public Dataset transform() {
+        dataset = DatasetFactory.createGeneral();
+        final Map<String, String> namespaces = setupNamespaces();
+
+        mainGraphIri = EccairsUtils.getVersionedOntologyUrl(base, dictionary.getTaxonomy(),
+            dictionary.getVersion());
+        mappingGraphIri = mainGraphIri + "/mapping";
+
         model = ModelFactory.createOntologyModel();
-        model.setNsPrefix("e-m", Vocabulary.ONTOLOGY_IRI_model + "/");
+        dataset.addNamedModel(mainGraphIri, model);
+        namespaces.forEach((k, v) -> model.setNsPrefix(k, v));
+
+        mapping = ModelFactory.createDefaultModel();
+        namespaces.forEach((k, v) -> mapping.setNsPrefix(k, v));
+        dataset.addNamedModel(mappingGraphIri, mapping);
 
         transformDictionary();
 
-        model.setNsPrefix("e-" + dictionary.getTaxonomy().toLowerCase().replace("ECCAIRS ","").charAt(0) + "-" + dictionary.getVersion(),
-            getOntologyIriBase());
         dictionary.getEntities().forEach(this::transformEntity);
 
-        return model;
+        return dataset;
     }
 
     private void transformDictionary() {
         this.lang = getLang(dictionary.getLanguage());
-        final String prefix = generateOntologyPrefix(dictionary);
-        model.setNsPrefix(prefix, getOntologyIriBase());
-        final Ontology o = model.createOntology(getOntologyIri());
-        o.addProperty(model.createAnnotationProperty(Vocabulary.s_p_has_key), dictionary.getKey());
+        final Ontology o = model.createOntology(getVersionedOntologyIri());
+        o.addProperty(ap(Vocabulary.s_p_has_key), dictionary.getKey());
     }
 
     private AnnotationProperty ap(final String uri) {
@@ -105,7 +152,7 @@ public class EccairsTaxonomyToRdf {
                 child);
         model.add(s);
         final ReifiedStatement rs = s.createReifiedStatement(
-            getOntologyIriBase() + "r-" +
+            getVersionedOntologyIriBase() + "r-" +
                 parent.getURI().substring(parent.getURI().lastIndexOf("-") - 1)
                 + "-" + child.getURI()
                 .substring(child.getURI().lastIndexOf("-") - 1));
@@ -129,9 +176,11 @@ public class EccairsTaxonomyToRdf {
 
     private Individual transformEntity(final EccairsEntity entity) {
         final OntClass cEntity = model.createClass(Vocabulary.s_c_entity);
+        final String localName = "e-" + entity.getId();
         final Individual rEntity =
-            model.createIndividual(getOntologyIriBase() + "e-" + entity.getId(), cEntity);
+            model.createIndividual(getVersionedOntologyIriBase() + localName, cEntity);
         addProperties(rEntity, entity);
+        addMapping(localName);
         Optional.ofNullable(entity.getEntities()).orElse(Collections.emptySet())
             .forEach(subEntity -> {
                 final Individual rChild = transformEntity(subEntity);
@@ -149,13 +198,16 @@ public class EccairsTaxonomyToRdf {
 
     private Individual transformAttribute(final EccairsAttribute attribute) {
         final OntClass cAttribute = model.createClass(Vocabulary.s_c_attribute);
+        final String localName = "a-" + attribute.getId();
         final Individual rAttribute =
-            model.createIndividual(getOntologyIriBase() + "a-" + attribute.getId(), cAttribute);
+            model.createIndividual(getVersionedOntologyIriBase() + localName, cAttribute);
         addProperties(rAttribute, attribute);
+        addMapping(localName);
         if (!Optional.ofNullable(attribute.getValues()).orElse(Collections.emptyList()).isEmpty()) {
             final OntClass cValueList = model.createClass(Vocabulary.s_c_value_list);
             final String valueListId = "vl-a-" + attribute.getId();
-            final String valueListUri = getOntologyIriBase() + valueListId;
+            final String valueListUri = getVersionedOntologyIriBase() + valueListId;
+            addMapping(valueListId);
             final Individual rValueList =
                 model.createIndividual(valueListUri, cValueList);
             createSubValueLink(rAttribute, rValueList);
@@ -182,15 +234,22 @@ public class EccairsTaxonomyToRdf {
         return rValue;
     }
 
+    private void addMapping(final String termLocalName) {
+        final Resource versionedResource =
+            mapping.getResource(getVersionedOntologyIriBase() + termLocalName);
+        final Resource unversionedResource =
+            mapping.getResource(getOntologyIriBase() + termLocalName);
+        mapping.add(versionedResource,
+            mapping.getProperty("http://onto.fel.cvut.cz/ontologies/eccairs/model/is-version-of"),
+            unversionedResource);
+    }
+
     private static String getLang(String language) {
-        switch (language) {
-            case "English":
-                return "en";
-            case "Czech":
-                return "cs";
-            default:
-                throw new IllegalArgumentException("Unsupported language '" + language + "' ");
+        final String languageTag = EccairsUtils.getLangCodeForEccairsLanguage(language);
+        if (languageTag == null) {
+            throw new IllegalArgumentException("Unsupported language '" + language + "' ");
         }
+        return languageTag;
     }
 
     private void addProperties(final Resource resource, final EccairsEntity entity) {
@@ -207,33 +266,43 @@ public class EccairsTaxonomyToRdf {
     }
 
     private void addProperties(final Resource resource, final EccairsValue value) {
-        resource.addProperty(model.createDatatypeProperty(Vocabulary.s_p_has_level),
-            value.getLevel());
+        resource.addProperty(dp(Vocabulary.s_p_has_level), value.getLevel());
         addTermProperties(resource, value);
     }
 
     private void addTermProperties(final Resource resource, final EccairsTerm term) {
         resource.addProperty(RDFS.label, ResourceFactory
             .createLangLiteral(term.getId() + " - " + term.getDescription(), lang));
-        resource.addProperty(model.createDatatypeProperty(Vocabulary.s_p_has_id),
+        resource.addProperty(dp(Vocabulary.s_p_has_id),
             ResourceFactory.createTypedLiteral(term.getId()));
-        resource.addProperty(model.createDatatypeProperty(Vocabulary.s_p_has_description),
+        resource.addProperty(dp(Vocabulary.s_p_has_description),
             ResourceFactory.createLangLiteral(term.getDescription(), lang));
-        resource.addProperty(model.createDatatypeProperty(Vocabulary.s_p_has_detailed_description),
+        resource.addProperty(dp(Vocabulary.s_p_has_detailed_description),
             ResourceFactory.createLangLiteral(term.getDetailedDescription(), lang));
-        resource.addProperty(model.createDatatypeProperty(Vocabulary.s_p_has_explanation),
+        resource.addProperty(dp(Vocabulary.s_p_has_explanation),
             ResourceFactory.createLangLiteral(term.getExplanation(), lang));
+    }
+
+    private String getVersionedOntologyIriBase() {
+        return getVersionedOntologyIri() + "/";
     }
 
     private String getOntologyIriBase() {
         return getOntologyIri() + "/";
     }
 
-    private String getOntologyIri() {
-        return EccairsUtils.getOntologyUrl(
+    private String getVersionedOntologyIri() {
+        return EccairsUtils.getVersionedOntologyUrl(
             this.base,
             dictionary.getTaxonomy(),
             dictionary.getVersion()
+        );
+    }
+
+    private String getOntologyIri() {
+        return EccairsUtils.getOntologyUrl(
+            this.base,
+            dictionary.getTaxonomy()
         );
     }
 
@@ -241,7 +310,7 @@ public class EccairsTaxonomyToRdf {
         final EccairsTaxonomyParser p = new EccairsTaxonomyParser();
         final EccairsDictionary d = p.parse(new File(args[0]));
         final EccairsTaxonomyToRdf r = new EccairsTaxonomyToRdf("http://test.org/", d);
-        final OntModel model = r.transform();
-        RDFDataMgr.write(new FileOutputStream(args[0] + ".trig"), model, RDFFormat.TRIG_PRETTY);
+        final Dataset dataset = r.transform();
+        RDFDataMgr.write(new FileOutputStream(args[0] + ".trig"), dataset, RDFFormat.TRIG_PRETTY);
     }
 }
